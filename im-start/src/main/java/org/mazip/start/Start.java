911140844c5fba5c89aae55bd81844a058e933fa
mazip <314059610@qq.com>
2016-08-19 11:04:37 +0800

    -

---
+++ im-start/src/main/java/org/mazip/start/Start.java
@@ -0,0 +1,44 @@
+package org.mazip.start;
+
+/**
+ * Created by mazip on 2016/8/19.
+ */
+public class Start {
+
+
+    /**
+     * 项目启动
+     * @param args
+     */
+    public static void main(String[] args) {
+        //检查配置项
+        checkConf();
+        //加载模块
+        loadMod();
+        //启动
+        doStart();
+    }
+
+    /**
+     * 检查配置项
+     */
+    private static void checkConf(){
+
+    }
+
+    /**
+     * 加载模块
+     */
+    private static void loadMod(){
+
+    }
+
+    /**
+     * 启动
+     */
+    private static void doStart(){
+
+    }
+
+
+}
